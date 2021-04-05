b82f8f744a672ffad529a29bbc836b803df2b977
Tomas <Aquista35@gmail.com>
2021-04-05 18:42:06 -0300

    Estela, fade, random de los planetas

---
+++ Aquistapace Tomas - Vectores/Assets/Scripts/MathDebbuger/PlanesTarea.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CustomMath;
+
+public class PlanesTarea : MonoBehaviour
+{
+    Vec3 front1 = new Vec3(0,0,0);
+    Vec3 front2 = new Vec3(0,0,0);
+    Vec3 back1 = new Vec3(0,0,0);
+    Vec3 back2 = new Vec3(0,0,0);
+    Vec3 down1 = new Vec3(0,0,0);
+    Vec3 down2 = new Vec3(0,0,0);
+    Vec3 left1 = new Vec3(0,0,0);
+    Vec3 left2 = new Vec3(0,0,0);
+    Vec3 right1 = new Vec3(0,0,0);
+    Vec3 right2 = new Vec3(0,0,0);
+    Vec3 top1 = new Vec3(0,0,0);
+    Vec3 top2 = new Vec3(0,0,0);
+
+    Plane plane;
+
+    void Start()
+    {
+
+        //plane.SetNormalAndPosition((0, 0, 0), (0, 0, 0));
+
+        
+
+    }
+}
